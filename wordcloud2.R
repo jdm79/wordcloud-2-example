9dc9b15e995a7d7f5e4abbffd491b2893a7cac94
jdm79 <james.malvern@gmail.com>
2020-03-28 21:10:47 +0000

    creates a black background, random-light color

---
+++ wordcloud2.R
@@ -0,0 +1,60 @@
+install.packages("rtweet")
+install.packages("tidytext")
+install.packages("dplyr")
+install.packages("stringr")
+require(devtools)
+install_github("lchiffon/wordcloud2")
+
+library(tidytext)
+library(dplyr)
+library(stringr)
+library(rtweet)
+library(wordcloud2)
+
+#Create twitter token
+#replace with your values
+create_token(
+  app = "PLACE YOUR APP NAME HERE",
+  consumer_key = "PLACE YOUR CONSUMER KEY HERE",
+  consumer_secret = "PLACE YOUR CONSUMER SECRET HERE")
+
+############# Handmaiden's Tale Tweets #############
+
+#Grab tweets - note: reduce to 1000 if it's slow
+?search_tweets
+hmt <- search_tweets(
+  "#5G", n = 2000, include_rts = FALSE
+)
+
+#Look at tweets
+head(hmt)
+dim(hmt)
+hmt$text
+
+#Unnest the words - code via Tidy Text
+
+hmtTable <- hmt %>% 
+  unnest_tokens(word, text)
+
+#remove stop words - aka typically very common words such as "the", "of" etc
+
+data(stop_words)
+
+hmtTable <- hmtTable %>%
+  anti_join(stop_words)
+
+#do a word count
+
+hmtTable <- hmtTable %>%
+  count(word, sort = TRUE) 
+hmtTable 
+
+#Remove other nonsense words
+hmtTable <-hmtTable %>%
+  filter(!word %in% c('t.co', 'https', 'handmaidstale', "handmaid's", 'season', 'episode', 'de', 'handmaidsonhulu',  'tvtime', 
+                      'watched', 'watching', 'watch', 'la', "it's", 'el', 'en', 'tv',
+                      'je', 'ep', 'week', 'amp'))
+
+
+#wordcloud2
+wordcloud2(hmtTable, backgroundColor = "black", color = "random-light")
